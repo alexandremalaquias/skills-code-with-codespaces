b8a0566ff4fa436d99a2e8c5a9e34830fb017000
Alexandre Marques Menezes Malaquias <a.menezes.malaquias@avanade.com>
2024-03-08 10:45:30 -0300

    teste blame

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Check if the system is running on Linux
-if [[ "$(uname)" == "Linux" ]]; then
-    # Get the Linux distribution name and version
-    if [[ -f /etc/os-release ]]; then
-        source /etc/os-release
-        echo "Linux distribution: $NAME"
-        echo "Version: $VERSION"
+
+# Determine OS version
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "Mac OS $(sw_vers -productVersion)"
+elif [[ "$(uname)" == "Linux" ]]; then
+    if [[ -f "/etc/os-release" ]]; then
+        source "/etc/os-release"
+        echo -n "$PRETTY_NAME "
+        echo -n "$(grep -oP '(?<=VERSION_ID=)\d+' /etc/os-release)"
+    elif [[ -f "/etc/lsb-release" ]]; then
+        source "/etc/lsb-release"
+        echo -n "$DISTRIB_DESCRIPTION "
+        echo -n "$(grep -oP '(?<=DISTRIB_RELEASE=)\d+' /etc/lsb-release)"
     else
-        echo "Unable to determine Linux distribution"
+        echo "Linux"
     fi
 else
-    echo "This script is intended to run on Linux only"
+    echo "Unknown OS"
 fi
 
 
