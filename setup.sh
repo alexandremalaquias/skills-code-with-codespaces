091225bae6339559bdde230491821e86233791da
Alexandre Marques Menezes Malaquias <a.menezes.malaquias@avanade.com>
2024-03-08 10:43:12 -0300

    teste blame

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install sl
+# Check if the system is running on Linux
+if [[ "$(uname)" == "Linux" ]]; then
+    # Get the Linux distribution name and version
+    if [[ -f /etc/os-release ]]; then
+        source /etc/os-release
+        echo "Linux distribution: $NAME"
+        echo "Version: $VERSION"
+    else
+        echo "Unable to determine Linux distribution"
+    fi
+else
+    echo "This script is intended to run on Linux only"
+fi
+
+
